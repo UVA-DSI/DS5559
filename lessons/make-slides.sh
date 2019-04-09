aa420e1cb42a9c51afbc0a02992636ba5d04861f
ontoligent <ontoligent@gmail.com>
2019-04-09 08:49:14 -0400

    Changes for lecture

--- lessons/make-slides.sh
+++ lessons/make-slides.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+# Transitions: none/fade/slide/convex/concave/zoom
+
 echo "$1.md -> $1.html"
 #pandoc -t revealjs -s -o $1.html $1.md -V revealjs-url=../../lib/reveal
-pandoc -t revealjs -s -o $1.html $1.md -V revealjs-url=https://revealjs.com
+pandoc -t revealjs -s -o $1.html $1.md \
+    -V revealjs-url=https://revealjs.com \
+    -V theme=simple \
+    -V transition=none \
+    -V controls=true \
+    -V progress=true \
+    -V slideNumber=true \
+    --css default-pandoc-slides.css \
+    --slide-level=1
